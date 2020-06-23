df4b3178e62644198da7032ff95338b55963bce7
Mercedes <mercedes.nmx@gmail.com>
2020-06-23 10:07:31 -0600

    removed null

--- src/main/java/edu/cnm/deepdive/quotes/model/entity/Quote.java
+++ src/main/java/edu/cnm/deepdive/quotes/model/entity/Quote.java
@@ -20,19 +20,17 @@ import org.springframework.lang.NonNull;
 @Entity
 public class Quote {
 
-  @NonNull
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   @Column(name = "quote_id", nullable = false, updatable = false)
   private Long id;
 
-  @NonNull
   @CreationTimestamp
   @Temporal(TemporalType.TIMESTAMP)
   @Column(nullable = false, updatable = false)
   private Date created;
 
-  @NonNull
+
   @UpdateTimestamp
   @Temporal(TemporalType.TIMESTAMP)
   @Column(nullable = false)
@@ -48,22 +46,18 @@ public class Quote {
   @JoinColumn(name = "source_id")
   private Source source;
 
-  @NonNull
   public Long getId() {
     return id;
   }
 
-  @NonNull
   public Date getCreated() {
     return created;
   }
 
-  @NonNull
   public Date getUpdated() {
     return updated;
   }
 
-  @NonNull
   public String getText() {
     return text;
   }
