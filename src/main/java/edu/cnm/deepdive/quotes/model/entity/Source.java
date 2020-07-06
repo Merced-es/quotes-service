817fd8bdf1b303ce1de664b78a15ba980c236aac
Mercedes <mercedes.nmx@gmail.com>
2020-07-06 10:09:19 -0600

    commit

--- src/main/java/edu/cnm/deepdive/quotes/model/entity/Source.java
+++ src/main/java/edu/cnm/deepdive/quotes/model/entity/Source.java
@@ -1,11 +1,17 @@
 package edu.cnm.deepdive.quotes.model.entity;
 
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import org.hibernate.annotations.CreationTimestamp;
@@ -35,9 +41,14 @@ public class Source {
   @Column(nullable = false)
   private Date updated;
 
-  public Long getId() {
-    return id;
-  }
+  @OneToMany(fetch = FetchType.LAZY,
+      mappedBy = "",
+      cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH}
+      )
+  @OrderBy("text ASC")
+  private List<Quote> quotes = new LinkedList<>();
+
+  public Long getId() { return id; }
 
   @NonNull
   public String getName() {
@@ -56,4 +67,7 @@ public class Source {
     return updated;
   }
 
+  public List<Quote> getQuotes() {
+    return quotes;
+  }
 }
