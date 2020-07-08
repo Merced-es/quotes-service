be8175d4cf6ba9a4cfdaa7c2e932eca41b8abcde
Mercedes <mercedes.nmx@gmail.com>
2020-07-08 17:03:51 -0600

    added mapping

--- src/main/java/edu/cnm/deepdive/quotes/controller/QuoteController.java
+++ src/main/java/edu/cnm/deepdive/quotes/controller/QuoteController.java
@@ -14,8 +14,10 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -66,4 +68,14 @@ public class QuoteController {
     return quoteRepository.findById(id).orElseThrow(NoSuchElementException::new);
   }
 
-}
+  @GetMapping(value = "/search", produces = MediaType.APPLICATION_JSON_VALUE)
+  public Iterable<Quote> search(@RequestParam(name = "q", required = true) String filter) {
+    return quoteRepository.getAllByTagsContainingOrderByTextAsc(filter);
+  }
+
+  @PutMapping(value = "/{id:\\d+}/text",
+      produces = MediaType.TEXT_PLAIN_VALUE, consumes = MediaType.TEXT_PLAIN_VALUE)
+  public String putText(@PathVariable long id, @RequestBody String text) {
+
+
+  }
