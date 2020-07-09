72e0c72c54b1477815695653209056d66f2d08d8
Mercedes <mercedes.nmx@gmail.com>
2020-07-09 16:34:09 -0600

    Integrated HATEOAS for resource URLs.

--- src/main/java/edu/cnm/deepdive/quotes/QuotesApplication.java
+++ src/main/java/edu/cnm/deepdive/quotes/QuotesApplication.java
@@ -1,13 +1,23 @@
 package edu.cnm.deepdive.quotes;
 
+import edu.cnm.deepdive.quotes.model.entity.Source;
+import edu.cnm.deepdive.quotes.model.entity.Tag;
+import java.util.Date;
+import java.util.List;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
+import org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType;
+import org.springframework.lang.NonNull;
 
 @SpringBootApplication
+@EnableHypermediaSupport(type = HypermediaType.HAL)
 public class QuotesApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(QuotesApplication.class, args);
   }
+
 
 }
